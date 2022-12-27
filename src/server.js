186d6298937f0c764183984f117177267967b099
ambrosius calvin <calvambro@gmail.com>
2022-12-27 10:56:09 +0700

    change localhost

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: '192.168.1.3',
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
